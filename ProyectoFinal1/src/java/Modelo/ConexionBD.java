6000abe9e228349ce87bfcc0cfa973ee9f29110f
Scarlett <Scarlett@Scarlett>
2016-05-20 14:50:20 -0500

    mmm

---
+++ ProyectoFinal1/src/java/Modelo/ConexionBD.java
@@ -0,0 +1,122 @@
+
+
+package ejemplobd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class ConexionBD {
+    
+    Connection con = null;
+    public ConexionBD()
+    {
+        realizarConexion();
+        //registrarEstudiante("123456","Gustavo","Barranca");
+        //consultarEstudiante("123");
+        //actualizarEstudiante("123","Pancha Carrazco","Alajuelita es mi Cantón");
+        eliminarEstudiante("123");
+    }
+    public void realizarConexion()
+    {
+        try {
+            String userName = "root";
+            String password = "root";
+            String url = "jdbc:mysql://localhost:3306/matricula";
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(url, userName, password);
+            System.out.println("Conexión Realizada");
+        } catch (Exception e) {
+            System.err.println("Cannot connect to database server");
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        } 
+    }
+    public boolean registrarEstudiante(String cedula, String nombre, String direccion)
+    {
+        ResultSet rs = null;
+        Statement cmd = null;
+        boolean ejecuto;
+        try {
+                cmd = con.createStatement();
+                ejecuto = cmd.execute("INSERT INTO estudiantes(cedula,nombre,direccion) VALUES ('"+cedula+"','"+nombre+"','"+direccion+"')");
+                
+               return true;
+               // rs.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("SQLException ejecutando sentencia: " + e.getMessage());
+            return false;
+        }
+        
+    }
+    
+    public boolean actualizarEstudiante(String cedula, String nombre, String direccion)
+    {
+        ResultSet rs = null;
+        Statement cmd = null;
+        boolean ejecuto;
+        try {
+                cmd = con.createStatement();
+                ejecuto = cmd.execute("UPDATE `estudiantes` SET cedula='"+cedula+"',nombre='"+nombre+"',direccion='"+direccion+"' WHERE cedula='"+cedula+"'"); 
+               return true;
+               // rs.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("SQLException ejecutando sentencia: " + e.getMessage());
+            return false;
+        }     
+    }
+    
+    public boolean eliminarEstudiante(String cedula)
+    {
+        ResultSet rs = null;
+        Statement cmd = null;
+        boolean ejecuto;
+        try {
+                cmd = con.createStatement();
+                ejecuto = cmd.execute("DELETE FROM `estudiantes` WHERE cedula='"+cedula+"'"); 
+               return true;
+               // rs.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("SQLException ejecutando sentencia: " + e.getMessage());
+            return false;
+        }     
+    }
+    
+    public void consultarEstudiante(String cedula)
+    {
+        ResultSet rs = null;
+        Statement cmd = null;
+
+        try {
+                cmd = con.createStatement();
+                rs = cmd.executeQuery("SELECT * FROM estudiantes where cedula='"+cedula+"'");
+                
+                while (rs.next()) 
+                {
+                    String nombre = rs.getString("nombre");
+                    String direccion=rs.getString("direccion");
+                    
+                    System.out.println("Información de la BD: Nombre: "+nombre+" direccion: "+direccion); 
+                }
+                rs.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("SQLException ejecutando sentencia: " + e.getMessage());
+        }
+        
+    }
+    
+}
